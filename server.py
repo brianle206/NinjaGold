d3778b3cd0e637fd72486e204a85904546e4cc26
Brian Le <brianle206@gmail.com>
2015-11-06 16:52:02 -0800

    Added css and added style

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, render_template, session, request
 import random
+from datetime import datetime
+time = datetime.now()
 app = Flask(__name__)
 app.secret_key = "Top_Secret"
 
@@ -8,21 +10,40 @@ app.secret_key = "Top_Secret"
 def index():
 	try:
 		session['gold']
+		session['msg']
 	except:
 		session['gold'] = 0
-	
-	return render_template("index.html", gold=session['gold'])
+		session['msg'] = []
+ 	return render_template("index.html", gold=session['gold'], msg=session['msg'])
 
 @app.route('/process_money', methods=['POST'])
 def datmonie():
 	if request.form['building'] == 'farm':
-		session['gold'] += random.randrange(10,20)
+		session['makemoney1'] = random.randrange(10,21)
+		session['gold'] += session['makemoney1']
+		note = "Earned %d golds from farming shit %s" %(session['makemoney1'], time)
+		session['msg'].append(note)
 	elif request.form['building'] == 'cave':
-		session['gold'] += random.randrange(5,10)
+		session['makemoney2'] = random.randrange(5,11)
+		session['gold'] += session['makemoney2']
+		note = "Earned %d golds from caving shit %s" %(session['makemoney2'], time)
+		session['msg'].append(note)
 	elif request.form['building'] == 'house':
-		session['gold'] += random.randrange(2,5)
+		session['makemoney3'] = random.randrange(2,6)
+		session['gold'] += session['makemoney3']
+		note = "Earned %d golds from housing shit %s" %(session['makemoney3'], time)
+		session['msg'].append(note)
 	else:
-		session['gold'] += random.randrange(0,50)
+		session['makemoney4'] = random.randrange(-50,50)
+		session['gold'] += session['makemoney4']
+		if session['makemoney4'] > 0 :
+			note = "Yo youz lucky %d golds from casinoing shit %s" %(session['makemoney4'], time)
+			session['msg'].append(note)
+
+		else:
+			note = "You Suck Yo wife gonna kill you, lost %d golds mang %s" %(session['makemoney4'], time)
+			session['msg'].append(note)
+
 	return redirect('/')
 
 app.run(debug=True)
